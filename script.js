f443e2f906965a3fda5bc8ab7becf83b7ee48686
GOWTHAM M H <gowthammh02@gmail.com>
2023-08-02 22:39:17 +0530

    cart

---
+++ script.js
@@ -0,0 +1,39 @@
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+
+var loginbutton = document.querySelector('.loginbtn');
+var signupbutton = document.querySelector('.sighupbtn');
+
+
+loginbutton.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = "./login.html";
+});
+
+signupbutton.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = "./signup.html";
+});
+
+var myCart = document.querySelector('.myCart');
+
+myCart.addEventListener('click', (e) => {
+    if (!sessionStorage.getItem('currentUserObj')) {
+        alert('Login to your Account First');
+    }
+    else {
+        window.location.href = "/cart.html"
+    }
+})
+
+var myProfile = document.querySelector('.profile');
+
+myProfile.addEventListener('click', (e) => {
+    if (!sessionStorage.getItem('currentUserObj')) {
+        alert('Login to your Account First');
+    }
+    else {
+        window.location.href = "/profile.html"
+    }
+})
